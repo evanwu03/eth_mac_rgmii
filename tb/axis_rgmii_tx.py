4d605fca9876b304e7246a2c99c05152b4d5c467
Evan Wu <ewu43951@gmail.com>
2026-06-18 17:10:34 -0500

    modify testbench to only monitor slave

--- tb/axis_rgmii_tx.py
+++ tb/axis_rgmii_tx.py
@@ -14,9 +14,8 @@ sys.path.insert(0, str(TB_DIR))
 import cocotb
 from cocotb.triggers import RisingEdge
 from cocotb.clock import Clock
-from cocotb.regression import TestFactory
 from cocotb_tools.runner import get_runner
-from cocotbext.axi import (AxiStreamFrame, AxiStreamBus, AxiStreamSource, AxiStreamSink, AxiStreamMonitor)
+from cocotbext.axi import (AxiStreamFrame, AxiStreamBus, AxiStreamSource, AxiStreamMonitor)
 from cocotb.handle import Immediate
 
 
@@ -35,29 +34,30 @@ def incrementing_payload(length):
     return bytearray(itertools.islice(itertools.cycle(range(256)), length))
 
 
-class TB: 
-    
-    def __init__(self, dut) -> None:
+class TB:
+    def __init__(self, dut):
         self.dut = dut
-
-        self.log = logging.getLogger("cocotb.tb")
-        self.log.setLevel(logging.DEBUG)
 
         cocotb.start_soon(Clock(dut.i_clk, 2, unit="ns").start())
 
-        self.source = AxiStreamSource(AxiStreamBus.from_prefix(dut, "s_axis"), dut.i_clk, dut.i_rst_n)
-        self.sink   = AxiStreamSink(AxiStreamBus.from_prefix(dut, "m_axis"), dut.i_clk, dut.i_rst_n)
-        self.monitor = AxiStreamMonitor(AxiStreamBus.from_prefix(dut, "s_axis"), dut.i_clk, dut.i_rst_n)
+        self.source = AxiStreamSource(
+            AxiStreamBus.from_prefix(dut, "s_axis"),
+            dut.i_clk,
+            dut.i_rst_n,
+            reset_active_level=False,
+        )
 
+        self.s_axis_monitor = AxiStreamMonitor(
+            AxiStreamBus.from_prefix(dut, "s_axis"),
+            dut.i_clk,
+            dut.i_rst_n,
+            reset_active_level=False,
+        )
 
-    def set_idle_generator(self, generator=None) -> None:
+    def set_idle_generator(self, generator=None):
         if generator:
             self.source.set_pause_generator(generator())
 
-
-    def set_backpressure_generator(self, generator=None):
-        if generator:
-            self.sink.set_pause_generator(generator())
 
     async def reset(self):
         self.dut.i_rst_n.set(Immediate(1))
@@ -71,62 +71,35 @@ class TB:
             await RisingEdge(self.dut.i_clk)
 
 
+
 @cocotb.test(timeout_time=10, timeout_unit="us")
 @cocotb.parametrize(
     payload_lengths=[size_list],
     payload_data=[incrementing_payload],
     idle_inserter=[None, cycle_pause],
-    backpressure_inserter=[None, cycle_pause]
 )
-async def run_test(dut, payload_lengths=None, payload_data=None, idle_inserter=None, backpressure_inserter=None):
+async def run_test(dut, payload_lengths=None, payload_data=None, idle_inserter=None):
 
     tb = TB(dut)
 
-    #id_count = 2**len(tb.source.bus.tid)
-
-    #cur_id = 1
-
     await tb.reset()
-
     tb.set_idle_generator(idle_inserter)
-    tb.set_backpressure_generator(backpressure_inserter)
 
     test_frames = []
 
     for test_data in [payload_data(x) for x in payload_lengths()]:
-        test_frame = AxiStreamFrame(test_data)
-        #test_frame.tid = cur_id
-        #test_frame.tdest = cur_id
-        await tb.source.send(test_frame)
-
-        test_frames.append(test_frame)
-
-        #cur_id = (cur_id + 1) % id_count
+        frame = AxiStreamFrame(test_data)
+        await tb.source.send(frame)
+        test_frames.append(frame)
 
     for test_frame in test_frames:
-        rx_frame = await tb.sink.recv()
+        mon_frame = await tb.s_axis_monitor.recv()
+        assert mon_frame.tdata == test_frame.tdata
 
-        #assert rx_frame.tdata == test_frame.tdata
-        #assert rx_frame.tid == test_frame.tid
-        #assert rx_frame.tdest == test_frame.tdest
-        #assert not rx_frame.tuser
+    assert tb.s_axis_monitor.empty()
 
-        mon_rx_frame = await tb.monitor.recv()
-
-        assert mon_rx_frame.tdata == test_frame.tdata
-        #assert mon_rx_frame.tid == test_frame.tid
-        #assert mon_rx_frame.tdest == test_frame.tdest
-        #assert not mon_rx_frame.tuser
-
-        assert rx_frame.sim_time_start == mon_rx_frame.sim_time_start
-        assert rx_frame.sim_time_end == mon_rx_frame.sim_time_end
-
-    assert tb.sink.empty()
-    assert tb.monitor.empty()
-
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)
-
+    await RisingEdge(dut.i_clk)
+    await RisingEdge(dut.i_clk)
 
 
 
@@ -143,7 +116,7 @@ def test_axis_rgmii_tx_py():
 
     parameters = {}
 
-
+    parameters['DATA_W'] = 8
 
     runner.build(
         sources=sources,
