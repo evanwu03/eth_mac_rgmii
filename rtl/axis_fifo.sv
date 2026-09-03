c59f28b020a9a94fbf2055b9ee4f3fc1d4ef352d
evanwu03 <ewu43951@gmail.com>
2026-09-02 21:01:16 -0500

    Update FSM documentation

--- rtl/axis_fifo.sv
+++ rtl/axis_fifo.sv
@@ -6,136 +6,135 @@ module axis_fifo #(
     parameter int FIFO_DEPTH = 64,
     parameter int ADDR_W     = $clog2(FIFO_DEPTH)
 ) (
-    input  logic                i_clk,
-    input  logic                i_reset_n,
+    input logic i_clk,
+    input logic i_reset_n,
 
-    input  logic [DATA_W-1:0]   s_axis_tdata,
-    input  logic                s_axis_tvalid,
-    input  logic                s_axis_tlast,
-    output logic                s_axis_tready,
+    input  logic [DATA_W-1:0] s_axis_tdata,
+    input  logic              s_axis_tvalid,
+    input  logic              s_axis_tlast,
+    output logic              s_axis_tready,
 
-    output logic [DATA_W-1:0]   m_axis_tdata,
-    output logic                m_axis_tvalid,
-    output logic                m_axis_tlast,
-    input  logic                m_axis_tready,
+    output logic [DATA_W-1:0] m_axis_tdata,
+    output logic              m_axis_tvalid,
+    output logic              m_axis_tlast,
+    input  logic              m_axis_tready,
 
-    output logic [ADDR_W:0]     o_fifo_count
+    output logic [ADDR_W:0] o_fifo_count
 );
 
 
-    logic [DATA_W-1:0] mem_tdata [0:FIFO_DEPTH-1];
-    logic              mem_tlast [0:FIFO_DEPTH-1];
+  logic [DATA_W-1:0] mem_tdata       [0:FIFO_DEPTH-1];
+  logic              mem_tlast       [0:FIFO_DEPTH-1];
 
-    logic [ADDR_W:0] wr_addr;
-    logic [ADDR_W:0] rd_addr;
+  logic [  ADDR_W:0] wr_addr;
+  logic [  ADDR_W:0] rd_addr;
 
-    logic [ADDR_W:0] wr_next_addr;
-    logic [ADDR_W:0] rd_next_addr;
+  logic [  ADDR_W:0] wr_next_addr;
+  logic [  ADDR_W:0] rd_next_addr;
 
-    logic fifo_empty;
-    logic fifo_full;
+  logic              fifo_empty;
+  logic              fifo_full;
 
-    logic fifo_empty_next;
-    logic fifo_full_next;
+  logic              fifo_empty_next;
+  logic              fifo_full_next;
 
-    wire load_output;
-    wire wr_en;
+  wire               load_output;
+  wire               wr_en;
 
-    assign load_output = (!m_axis_tvalid || m_axis_tready) && !fifo_empty;
+  assign load_output = (!m_axis_tvalid || m_axis_tready) && !fifo_empty;
 
-    assign s_axis_tready = !fifo_full || load_output;
+  assign s_axis_tready = !fifo_full || load_output;
 
-    assign wr_en = s_axis_tvalid && s_axis_tready;
+  assign wr_en = s_axis_tvalid && s_axis_tready;
 
-    assign o_fifo_count = wr_addr - rd_addr;
+  assign o_fifo_count = wr_addr - rd_addr;
 
 
-    always_comb begin
-        wr_next_addr = wr_addr;
-        rd_next_addr = rd_addr;
+  always_comb begin
+    wr_next_addr = wr_addr;
+    rd_next_addr = rd_addr;
 
-        if (wr_en)
-            wr_next_addr = wr_addr + 1'b1;
+    if (wr_en) wr_next_addr = wr_addr + 1'b1;
 
-        if (load_output)
-            rd_next_addr = rd_addr + 1'b1;
-    end
+    if (load_output) rd_next_addr = rd_addr + 1'b1;
+  end
 
-    always_comb begin
-        fifo_empty_next = (wr_next_addr == rd_next_addr);
+  always_comb begin
+    fifo_empty_next = (wr_next_addr == rd_next_addr);
 
-        fifo_full_next =
+    fifo_full_next =
             (wr_next_addr[ADDR_W]     != rd_next_addr[ADDR_W]) &&
             (wr_next_addr[ADDR_W-1:0] == rd_next_addr[ADDR_W-1:0]);
-    end
+  end
 
-    always_ff @(posedge i_clk) begin
-        if (!i_reset_n) begin
-            fifo_empty <= 1'b1;
-            fifo_full  <= 1'b0;
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n) begin
+      fifo_empty <= 1'b1;
+      fifo_full  <= 1'b0;
+    end else begin
+      fifo_empty <= fifo_empty_next;
+      fifo_full  <= fifo_full_next;
+    end
+  end
+
+  // Slave logic
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n) begin
+      wr_addr <= '0;
+    end else if (wr_en) begin
+      mem_tdata[wr_addr[ADDR_W-1:0]] <= s_axis_tdata;
+      mem_tlast[wr_addr[ADDR_W-1:0]] <= s_axis_tlast;
+      wr_addr <= wr_next_addr;
+    end
+  end
+
+  // Master Logic
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n) begin
+      rd_addr       <= '0;
+      m_axis_tvalid <= 1'b0;
+      m_axis_tdata  <= '0;
+      m_axis_tlast  <= 1'b0;
+    end else begin
+      if (!m_axis_tvalid || m_axis_tready) begin
+        if (!fifo_empty) begin
+          m_axis_tvalid <= 1'b1;
+          m_axis_tdata  <= mem_tdata[rd_addr[ADDR_W-1:0]];
+          m_axis_tlast  <= mem_tlast[rd_addr[ADDR_W-1:0]];
+          rd_addr       <= rd_next_addr;
         end else begin
-            fifo_empty <= fifo_empty_next;
-            fifo_full  <= fifo_full_next;
+          m_axis_tvalid <= 1'b0;
+          m_axis_tlast  <= 1'b0;
         end
+      end
     end
-
-    // Slave logic
-    always_ff @(posedge i_clk) begin
-        if (!i_reset_n) begin
-            wr_addr <= '0;
-        end else if (wr_en) begin
-            mem_tdata[wr_addr[ADDR_W-1:0]] <= s_axis_tdata;
-            mem_tlast[wr_addr[ADDR_W-1:0]] <= s_axis_tlast;
-            wr_addr <= wr_next_addr;
-        end
-    end
-
-    // Master Logic
-    always_ff @(posedge i_clk) begin
-        if (!i_reset_n) begin
-            rd_addr       <= '0;
-            m_axis_tvalid <= 1'b0;
-            m_axis_tdata  <= '0;
-            m_axis_tlast  <= 1'b0;
-        end else begin
-            if (!m_axis_tvalid || m_axis_tready) begin
-                if (!fifo_empty) begin
-                    m_axis_tvalid <= 1'b1;
-                    m_axis_tdata  <= mem_tdata[rd_addr[ADDR_W-1:0]];
-                    m_axis_tlast  <= mem_tlast[rd_addr[ADDR_W-1:0]];
-                    rd_addr       <= rd_next_addr;
-                end else begin
-                    m_axis_tvalid <= 1'b0;
-                    m_axis_tlast  <= 1'b0;
-                end
-            end
-        end
-    end
+  end
 
 
 
 `ifdef FORMAL
-// Verifies internal invariants
+  // Verifies internal invariants
 
-reg f_past_valid;
+  reg f_past_valid;
 
-initial f_past_valid = 0;
-always @(posedge i_clk) begin
+  initial f_past_valid = 0;
+  always @(posedge i_clk) begin
     f_past_valid <= 1;
-end
+  end
 
-// AXI backpressure is not overflow
-always @(posedge i_clk) begin
+  // AXI backpressure is not overflow
+  always @(posedge i_clk) begin
     if (f_past_valid && i_reset_n) begin
-        if (s_axis_tvalid && !s_axis_tready) begin
-            assert(!wr_en);
-        end
+      if (s_axis_tvalid && !s_axis_tready) begin
+        assert (!wr_en);
+      end
     end
-end
+  end
 
 
-`endif 
+`endif
 
 endmodule
 
 `resetall
+
--- rtl/axis_gmii_tx.sv
+++ rtl/axis_gmii_tx.sv
@@ -6,51 +6,50 @@ module axis_gmii_tx #(
     parameter int ENABLE_PADDING = 1,
     parameter int MIN_FRAME_LENGTH = 64,
     parameter int PADDING_EN = 1
-)(
+) (
     input wire logic i_clk,
     input wire logic i_rst_n,
 
     // Note: tx_clk as specified in RGMII v1.3 spec will stay in top MAC module
 
     // Axis Interface
-    input  wire logic [DATA_W-1:0]   s_axis_tdata,
-    input  wire logic                s_axis_tvalid,
-    input  wire logic                s_axis_tlast,
-    output logic                     s_axis_tready,
+    input  wire logic [DATA_W-1:0] s_axis_tdata,
+    input  wire logic              s_axis_tvalid,
+    input  wire logic              s_axis_tlast,
+    output logic                   s_axis_tready,
 
     // GMII outputs
-    output wire logic [DATA_W-1:0]    gmii_txd,
-    output wire logic                 gmii_tx_en,
-    output wire logic                 gmii_tx_er,
+    output wire logic [DATA_W-1:0] gmii_txd,
+    output wire logic              gmii_tx_en,
+    output wire logic              gmii_tx_er,
 
     // Control
-    input  wire logic                 clk_enable,
-    input  wire logic                 mii_select,
+    input wire logic clk_enable,
+    input wire logic mii_select,
 
     // Configuration
-    input  wire logic [15:0]          cfg_tx_max_pkt_len = 16'd1518-1,
-    input  wire logic [7:0]           cfg_tx_ifg = 8'd12,
-    input  wire logic                 cfg_tx_enable,
+    input wire logic [15:0] cfg_tx_max_pkt_len = 16'd1518 - 1,
+    input wire logic [ 7:0] cfg_tx_ifg = 8'd12,
+    input wire logic        cfg_tx_enable,
 
     // Status
-    output wire                         start_packet,
-    output wire                         error_underflow
+    output wire start_packet,
+    output wire error_underflow
 );
 
 
-// check configuration
-if (DATA_W != 8)
-    $fatal(0, "Error: Interface width must be 8 (instance %m)");
+  // check configuration
+  if (DATA_W != 8) $fatal(0, "Error: Interface width must be 8 (instance %m)");
 
 
 
-typedef enum logic [7:0] {
+  typedef enum logic [7:0] {
     ETH_PRE = 8'h55,
     ETH_SFD = 8'hD5
-} eth_pre_t;
+  } eth_pre_t;
 
 
-typedef enum logic [2:0] {  
+  typedef enum logic [2:0] {
     STATE_IDLE,
     STATE_PREAMBLE,
     STATE_PAYLOAD,
@@ -58,72 +57,67 @@ typedef enum logic [2:0] {
     STATE_PAD,
     STATE_FCS,
     STATE_IFG
-} state_t;
+  } state_t;
 
 
-// State register 
-state_t state_reg = STATE_IDLE, state_next;
-
-
-
-// Datapath control signals
+  // State register
+  state_t state_reg = STATE_IDLE, state_next;
 
 
 
-always_comb begin
-    
+  // Datapath control signals
 
+  always_comb begin
+    state_next = STATE_IDLE;
     case (state_reg)
-        
-        STATE_IDLE: begin
-            
 
-        end
-
-        STATE_PREAMBLE: begin
-            
-
-        end
-
-        STATE_PAYLOAD: begin
-            
-        end
+      STATE_IDLE: begin
 
 
-        STATE_LAST: begin
-            
+      end
 
-        end
-
-        STATE_FCS: begin
-        
-        end
+      STATE_PREAMBLE: begin
 
 
-        STATE_IFG: begin
-            
-        end
+      end
+
+      STATE_PAYLOAD: begin
+
+      end
+
+      STATE_LAST: begin
 
 
-        default: begin
-            state_next = STATE_IDLE;
-        end
+      end
+
+      STATE_FCS: begin
+
+      end
+
+      STATE_IFG: begin
+
+      end
+
+      default: begin
+        state_next = STATE_IDLE;
+      end
     endcase
 
 
-end
+  end
 
 
 
-always_ff @(posedge i_clk) begin
-    
-    // load next state this cycle 
+  always_ff @(posedge i_clk) begin
+
+    // load next state this cycle
     state_reg <= state_next;
 
-end
+  end
 
 
 
 endmodule : axis_gmii_tx
 
 `resetall
+
